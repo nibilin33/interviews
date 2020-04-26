481df1840780e340f362dde7f5bb2a21a0bd738b
nibilin <819231957@qq.com>
2020-04-26 20:17:25 +0800

    create 287

---
+++ codes/287.寻找重复数.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=287 lang=javascript
+ *
+ * [287] 寻找重复数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let map = new Set();
+    for(let i=0;i<nums.length;i++) {
+        if(map.has(nums[i])) {
+            return nums[i];
+        }else{
+            map.add(nums[i]);
+        } 
+    }
+};
+// @lc code=end
+
