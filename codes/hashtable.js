58570d8aaf351d2eac31ad69c3e20da02a118a53
nibilin <819231957@qq.com>
2020-04-27 20:45:25 +0800

    create hashtable

---
+++ codes/hashtable.js
@@ -0,0 +1,38 @@
+class HashTable{
+    constructor(){
+
+    }
+    /**
+     * 哈希函数
+     * 作用：返回key在哈希表的中的下标位置
+     * 参数：key（String） size(哈希表长度)
+     */
+    hashcode(){
+
+    }
+    push(key,value){
+
+    }
+    get(key){
+
+    }
+    remove(key){
+
+    }
+    size(){
+
+    }
+    /**
+     * 哈希表扩容
+     * 参数limit哈希表新的长度。
+    */
+    resize(limit){
+
+    }
+    /**
+     * 打印哈希表
+     */
+    toString(){
+
+    }
+}
