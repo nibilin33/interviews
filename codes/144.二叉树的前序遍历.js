263efc8630a3782a6d347ab47eaba6629072b3f2
nibilin <819231957@qq.com>
2020-04-29 13:32:44 +0800

    add 144,617

---
+++ codes/144.二叉树的前序遍历.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let result = [];
+    const deep = (node)=>{
+        if(!node) return;
+        result.push(node.val);
+        deep(node.left);
+        deep(node.right);
+    }
+    deep(root);
+    return result;
+};
+// @lc code=end
+
---
+++ codes/617.合并二叉树.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=javascript
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    if(!t1 && !t2) return null;
+    if(t1 && !t2) return t1;
+    if(!t1 && t2) return t2;
+    let root = new TreeNode(t1.val+t2.val);
+    root.left = mergeTrees(t1.left,t2.left);
+    root.right = mergeTrees(t1.right,t2.right);
+    return root;
+};
+// @lc code=end
+
