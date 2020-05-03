832e7dbf94bb9fc8c6b8b9af38ac99d666afd5dd
nibilin <819231957@qq.com>
2020-05-03 12:15:10 +0800

    create 108

---
+++ codes/108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=javascript
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+// 二叉搜索树的中序遍历是一个升序序列,根据中序遍历得二叉树
+var sortedArrayToBST = function(nums) {
+    const createTree = (left,right)=>{
+      if(left>right) {
+          return null;
+      }
+      let p = Math.round((left+right)/2);
+      let root = new TreeNode(nums[p]);
+      root.left = createTree(left,p-1);
+      root.right = createTree(p+1,right);
+      return root;
+    }
+    return createTree(0,nums.length-1);
+};
+// @lc code=end
+
