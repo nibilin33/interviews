95aa6feda89f5d4a08db41d39e44fce5713da976
nibilin <819231957@qq.com>
2020-05-04 11:04:45 +0800

    add 965

---
+++ codes/965.单值二叉树.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=965 lang=javascript
+ *
+ * [965] 单值二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+    const deep = (node) =>{
+        if(!node) return true;
+        if(node.val !== root.val) {
+            return false;
+        }
+        return deep(node.left)&&deep(node.right);
+    }
+    return deep(root);
+};
+// @lc code=end
+
