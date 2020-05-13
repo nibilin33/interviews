07e45c48e207b3d389479e027f865c724199ca0b
nibilin <819231957@qq.com>
2020-05-13 15:08:24 +0800

    update

--- codes/常见手写题.js
+++ codes/常见手写题.js
@@ -361,3 +361,20 @@ var arr = [1,2,3,4,5,6,7,8,9,10];
 arr.sort(function(){
     return Math.random() - 0.5;
 });
+
+//25.判断两个变量是否一样     
+
+if (! Object.is) {
+  Object.is = function(x, y) {
+
+    // SameValue algorithm
+    if (x === y) { // Steps 1-5, 7-10
+      // Steps 6.b-6.e: +0 != -0
+      return x !== 0 || 1 / x === 1 / y;
+    } else {
+      // Step 6.a: NaN == NaN
+      return x !== x && y !== y;
+    }
+
+  }; 
+}
