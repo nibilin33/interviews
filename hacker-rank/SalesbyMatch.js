facdbad39544f5144aee983ad0fa211b073aded5
nibilin33 <nibilin33@gmail.com>
2022-09-28 20:25:56 +0800

    Create SalesbyMatch.js

---
+++ hacker-rank/SalesbyMatch.js
@@ -0,0 +1,33 @@
+/*
+There is a large pile of socks that must be paired by color. 
+Given an array of integers representing the color of each sock, 
+determine how many pairs of socks with matching colors there are.
+*/
+
+function sockMerchant(n, ar) {
+    // Write your code here
+    let colorMap = {};
+    let pairs = 0;
+    for(let i=0;i<n;i++) {
+        if(typeof colorMap[ar[i]]=== 'undefined') {
+            colorMap[ar[i]]=1;
+        }else{
+            colorMap[ar[i]]++;
+        }
+    }
+    Object.keys(colorMap).forEach((key)=>{
+        if(colorMap[key]%2!=0){
+            colorMap[key]=colorMap[key]-1;
+        }
+        pairs+=colorMap[key]/2;
+    });
+    return pairs;
+}
+
+function main() {
+    const n = 10;
+    const ar = [1,1,3,1,2,1,3,3,3,3];
+    const result = sockMerchant(n, ar); // 4
+    console.log(result);
+}
+main();
