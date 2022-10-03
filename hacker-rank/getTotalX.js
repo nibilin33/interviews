0b7e0e2b07b8b90dea9b61134f9fe95d6a2b1400
nibilin33 <nibilin33@gmail.com>
2022-10-03 15:36:22 +0800

    add two qa

---
+++ hacker-rank/getTotalX.js
@@ -0,0 +1,49 @@
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+// 找出存在几个数都能被数组整除
+function getTotalX(a, b) {
+    // Write your code here
+    const all = [...a,...b];
+    let group1 = a[0], group2 = b[0];
+    let possibleFactors = new Set();
+    let count = 0;
+    function getFacotors(value) {
+        for(let i=1;i<value;i++) {
+            if(value%i===0 && i> group1) {
+                possibleFactors.add(i);
+                if(value/i > group1) {
+                    possibleFactors.add(value/i);
+                }
+            }
+        }
+    }
+    getFacotors(group1);
+    getFacotors(group2);
+    possibleFactors.forEach((data)=>{
+        if(all.every((item)=>{
+            if(data>item) {
+                return data%item === 0;
+            }else{
+                return item%data === 0;
+            }
+        })){
+            count++;
+        }
+    })
+    return count;
+
+}
+
+function main() {
+    const a = [2,6];
+    const b = [24,36]; // 2
+    const result = getTotalX(a,b);
+    console.log(result);
+}
+main();
---
+++ hacker-rank/palindromeIndex.js
@@ -0,0 +1,63 @@
+/*
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+// 删除一个字母让这个字符串变成回文，返回删除的标志
+function palindromeIndex(s) {
+    // Write your code here
+    const n = s.length;
+    let i = 0, j= n-1;
+    let result = -1;
+    let lasti = i, lastj=j;
+    console.log(s);
+    while(i<j) {
+        // console.log(i,j);
+        if(s[i]===s[j]) {
+            i++;
+            j--;
+        }else{
+            if(result > -1) {
+                result = -1;
+                break;
+            }
+            if(s[i]===s[j-1]){
+                if(i+1<=j-2) {
+                    if(s[i+1]===s[j-2]){
+                        result = j;
+                        j--;
+                    }
+                }
+            }
+            if(s[i+1]===s[j] && result < 0) {
+                result = i;
+                i++;
+            }
+        }
+    }
+    return result;
+
+}
+
+function main() {
+    const arr = [
+        'aaab',
+//  'fyjwtatuieusvfqaeynaaiiaanyeaqfvsueutatwjyf',
+// 'qaaiyrpadovfjrmgkildtkseysejdtrpltptujlxxljutptlprtdjesyeskdlikgmrjfvodapryiaaq',
+// 'llhrxcreddwkcronujfkwbdswoowsdbwkfjunorckwdderxrhll',
+// 'qasfhkfcojhntlfkaydtepsfsleipymwsopposwmypielsfspetdykfltnhjocfkhfsaq',
+// 'broifqivnnvifiorb',
+// 'bglgcwnmpobohqefrglsaaaaslgrfeqhobopmnwcglgb',
+// 'bthvmywukfwrkslaiialskwfkuwymvhtb',
+// 'uxxdlselxmwyiguugtpsypfudffswvwyswyyiiyywsywvsffdufpysptguugiywmxlesldxxu',
+// 'rvscdpyljqglgmiktfndsmfnkgmubrruloqptgohsgneocoyyocoengshogtpqolurrbumgknfmsdntkimglgqjlypdcsvr',
+// 'qmdpbsswvmqtyhkobqeijjieqbokhytqmvwssbdmq'
+    ]
+    arr.forEach((s)=>{
+        const result = palindromeIndex(s);
+        console.log(result);
+    })
+ 
+}
+main();
