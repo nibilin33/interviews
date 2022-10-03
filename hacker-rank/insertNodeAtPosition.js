c7c06c56327dc3ad5e6779dee7ad3862f5bdef04
nibilin33 <nibilin33@gmail.com>
2022-10-03 23:48:22 +0800

    Create insertNodeAtPosition.js

---
+++ hacker-rank/insertNodeAtPosition.js
@@ -0,0 +1,37 @@
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function insertNodeAtPosition(llist, data, position) {
+    // Write your code here
+    let head = llist;
+    let point = llist;
+    let node = new SinglyLinkedListNode(data);
+    let count = 0;
+    while(head) {
+        let next = head.next;
+        count++;
+        if(count == position) {
+            head.next = node;
+            node.next = next;
+        }
+        head = next;
+    }
+    return point;
+}
