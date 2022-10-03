5e4ea57bbfe4da75d3492edd520406bce1b34442
nibilin33 <nibilin33@gmail.com>
2022-10-03 23:41:03 +0800

    Create Reversedoublylinked.js

---
+++ hacker-rank/Reversedoublylinked.js
@@ -0,0 +1,23 @@
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode next;
+ *     DoublyLinkedListNode prev;
+ * }
+ *
+ */
+
+function reverse(llist) {
+    // Write your code here
+    let head = llist;
+    let point = null;
+    while(head) {
+        let next = head.next;
+        head.next = point;
+        point = head;
+        head = next;
+    }
+    return point;
+}
